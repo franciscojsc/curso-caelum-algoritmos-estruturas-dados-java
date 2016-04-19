f1e674d66377e6b4a08c7fe5a96ea5019f0394f0
Francisco Chaves <francisco.jschaves@gmail.com>
2016-04-19 12:41:26 -0300

    Informa o tamanho de elementos da lista

---
+++ src/br/com/caelum/ed/TesteTamanhoLista.java
@@ -0,0 +1,25 @@
+package br.com.caelum.ed;
+
+public class TesteTamanhoLista {
+
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno();
+		Aluno a2 = new Aluno();
+		Aluno a3 = new Aluno();
+		
+		a1.setNome("Rafael");
+		a1.setNome("Paulo");
+		
+		Vetor lista = new Vetor();
+		
+		lista.adiciona(a1);
+		lista.adiciona(a2);
+		
+		System.out.println(lista.tamanho());
+		
+		lista.adiciona(a3);
+		
+		System.out.println(lista.tamanho());
+	}
+
+}
