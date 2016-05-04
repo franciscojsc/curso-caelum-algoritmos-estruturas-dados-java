58506122b1964afada67e438cb7371305c2ab623
Francisco Chaves <francisco.jschaves@gmail.com>
2016-05-04 00:58:23 -0300

    Update Adicionar no Inicio
    Adicionar na primeira posição da lista.

--- src/br/com/caelum/ed/listasligadas/ListaLigada.java
+++ src/br/com/caelum/ed/listasligadas/ListaLigada.java
@@ -5,9 +5,18 @@ public class ListaLigada {
 	private Celula primeira;
 	private Celula ultima;
 
-	private int totalDeElementos;
+	private int totalDeElementos = 0;
 
-	
+	public void adicionaNoComeco(Object elemento) {
+		Celula nova = new Celula(this.primeira, elemento);
+		this.primeira = nova;
+
+		if (this.totalDeElementos == 0) {
+			this.ultima = this.primeira;
+		}
+
+		totalDeElementos++;
+	}
 	
 	public String toString(){
 		
@@ -15,10 +24,10 @@ public class ListaLigada {
 			return"[]";
 		}
 		
-		StringBuilder builder = new StringBuilder();
+		StringBuilder builder = new StringBuilder("[");
 		Celula atual = primeira;
 		
-		for (int i = 0; i < totalDeElementos; i++) {
+		for (int i = 0; i < totalDeElementos - 1; i++) {
 			builder.append(atual.getElemento());
 			builder.append(", ");
 			atual = atual.getProxima();
--- src/br/com/caelum/ed/listasligadas/testes/TesteAdicionarNoInicio.java
+++ src/br/com/caelum/ed/listasligadas/testes/TesteAdicionarNoInicio.java
@@ -1,6 +1,5 @@
 package br.com.caelum.ed.listasligadas.testes;
 
-import br.com.caelum.ed.listasligadas.AlunoLista;
 import br.com.caelum.ed.listasligadas.ListaLigada;
 
 public class TesteAdicionarNoInicio {
@@ -8,9 +7,10 @@ public class TesteAdicionarNoInicio {
 	public static void main(String[] args) {
 
 		ListaLigada lista = new ListaLigada();
-		AlunoLista aluno = new AlunoLista();
+		lista.adicionaNoComeco("Rafael");
+		lista.adicionaNoComeco("Paulo");
 		
-		lista.adicionarNoComeco(aluno);
+		System.out.println(lista.toString());
 	}
 
 }
