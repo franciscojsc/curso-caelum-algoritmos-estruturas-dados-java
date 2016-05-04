08b81fcde9ff9691559b15edcf4399a9d7f989a0
Francisco Chaves <francisco.jschaves@gmail.com>
2016-05-04 00:47:38 -0300

    Pecorrendo a lista
    O método utiliza a referência temporária para uma célula que vai
    apontando para a próxima e imprimindo.

--- src/br/com/caelum/ed/listasligadas/ListaLigada.java
+++ src/br/com/caelum/ed/listasligadas/ListaLigada.java
@@ -4,17 +4,30 @@ public class ListaLigada {
 
 	private Celula primeira;
 	private Celula ultima;
-	
+
 	private int totalDeElementos;
+
 	
-	public void adicionarNoComeco(Object elemento){
-		Celula nova = new Celula(this.primeira,elemento);
+	
+	public String toString(){
 		
 		if(this.totalDeElementos == 0){
-			this.ultima = this.primeira;
+			return"[]";
 		}
 		
-		totalDeElementos++;
+		StringBuilder builder = new StringBuilder();
+		Celula atual = primeira;
+		
+		for (int i = 0; i < totalDeElementos; i++) {
+			builder.append(atual.getElemento());
+			builder.append(", ");
+			atual = atual.getProxima();
+		}
+		
+		builder.append(atual.getElemento());
+		builder.append("]");
+		
+		return builder.toString();
 	}
-	
+
 }
