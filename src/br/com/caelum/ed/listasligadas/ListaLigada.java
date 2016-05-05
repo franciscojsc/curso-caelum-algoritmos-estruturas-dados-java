0e8a935c21ed0beb1044a466f35108e8afe98ab5
Francisco Chaves <francisco.jschaves@gmail.com>
2016-05-05 15:46:12 -0300

    update adicionar no começo

--- src/br/com/caelum/ed/listasligadas/ListaLigada.java
+++ src/br/com/caelum/ed/listasligadas/ListaLigada.java
@@ -8,97 +8,113 @@ public class ListaLigada {
 	private int totalDeElementos = 0;
 
 	public void adicionaNoComeco(Object elemento) {
-		Celula nova = new Celula(this.primeira, elemento);
-		this.primeira = nova;
-
 		if (this.totalDeElementos == 0) {
-			this.ultima = this.primeira;
+			Celula nova = new Celula(elemento);
+			this.primeira = nova;
+			this.ultima = nova;
+		} else {
+			Celula nova = new Celula(this.primeira, elemento);
+			this.primeira.setAnterior(nova);
+			this.primeira = nova;
 		}
-
-		totalDeElementos++;
+		this.totalDeElementos++;
 	}
-	
+
 	public void adiciona(Object elemento) {
-		
+
 		if (this.totalDeElementos == 0) {
-		    this.adicionaNoComeco(elemento);
-		}else{
+			this.adicionaNoComeco(elemento);
+		} else {
 			Celula nova = new Celula(elemento);
 			this.ultima.setProxima(nova);
 			this.ultima = nova;
 			this.totalDeElementos++;
 		}
-		
+
 	}
-	
+
 	public void adiciona(int posicao, Object elemento) {
-		
+
 		if (posicao == 0) {
 			this.adicionaNoComeco(elemento);
-		}else if(posicao == this.totalDeElementos){
+		} else if (posicao == this.totalDeElementos) {
 			this.adiciona(elemento);
-		}else{
+		} else {
 			Celula anterior = this.pegaCelula(posicao - 1);
 			Celula nova = new Celula(anterior.getProxima(), elemento);
 			anterior.setProxima(nova);
 			this.totalDeElementos++;
 		}
-		
+
 	}
-	
-	private boolean posicaoOcupada(int posicao){
-		return posicao >=0 && posicao < this.totalDeElementos;
+
+	private boolean posicaoOcupada(int posicao) {
+		return posicao >= 0 && posicao < this.totalDeElementos;
 	}
-	
-	private Celula pegaCelula(int posicao){
-		if(!this.posicaoOcupada(posicao)){
+
+	private Celula pegaCelula(int posicao) {
+		if (!this.posicaoOcupada(posicao)) {
 			throw new IllegalArgumentException("Posição não existe");
 		}
-		
+
 		Celula atual = primeira;
 		for (int i = 0; i < posicao; i++) {
 			atual = atual.getProxima();
 		}
-		
+
 		return atual;
 	}
-	
-	public Object pega(int posicao){
+
+	public Object pega(int posicao) {
 		return this.pegaCelula(posicao).getElemento();
 	}
-	
-	public void removeDoComeco(){
-	
-		if(!this.posicaoOcupada(0)){
+
+	public void removeDoComeco() {
+
+		if (!this.posicaoOcupada(0)) {
 			throw new IllegalArgumentException("Posição não existe");
 		}
-		
+
 		this.primeira = this.primeira.getProxima();
 		this.totalDeElementos--;
-		
-		if(this.totalDeElementos == 0){
+
+		if (this.totalDeElementos == 0) {
 			this.ultima = null;
 		}
 	}
-	
-	public String toString(){
-		
-		if(this.totalDeElementos == 0){
-			return"[]";
+
+	public void removeDoFim() {
+		if (!this.posicaoOcupada(this.totalDeElementos - 1)) {
+			throw new IllegalArgumentException("Posição não existe");
 		}
-		
+		if (this.totalDeElementos == 1) {
+			this.removeDoComeco();
+		} else {
+			Celula penultima = this.ultima.getAnterior();
+			penultima.setProxima(null);
+			this.ultima = penultima;
+			this.totalDeElementos--;
+		}
+	}
+
+	public String toString() {
+
+		if (this.totalDeElementos == 0) {
+			return "[]";
+		}
+
 		StringBuilder builder = new StringBuilder("[");
 		Celula atual = primeira;
-		
+
 		for (int i = 0; i < totalDeElementos - 1; i++) {
 			builder.append(atual.getElemento());
 			builder.append(", ");
 			atual = atual.getProxima();
 		}
-		
+
 		builder.append(atual.getElemento());
 		builder.append("]");
-		
+
 		return builder.toString();
 	}
 
