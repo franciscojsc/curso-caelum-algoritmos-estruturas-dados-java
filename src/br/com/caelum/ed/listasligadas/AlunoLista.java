67af5074425ac1ac0fc0dc89842d72a38b9cd2ba
Francisco Chaves <francisco.jschaves@gmail.com>
2016-05-05 16:06:45 -0300

    update Aluno

--- src/br/com/caelum/ed/listasligadas/AlunoLista.java
+++ src/br/com/caelum/ed/listasligadas/AlunoLista.java
@@ -5,5 +5,29 @@ public class AlunoLista {
 	private String nome;
 	private int idade;
 	private AlunoLista proximo;
-	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public AlunoLista getProximo() {
+		return proximo;
+	}
+
+	public void setProximo(AlunoLista proximo) {
+		this.proximo = proximo;
+	}
+
 }
