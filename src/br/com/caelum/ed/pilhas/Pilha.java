974453f8395ae5fa366ae0864e4e01a408a4034b
Francisco Chaves <francisco.jschaves@gmail.com>
2016-05-25 11:51:29 -0300

    Pilha
    "Uma lista é uma estrutura mais poderosa e mais genérica do que uma
    pilha". 
    Dentro desta Pilha séra utilizada uma LinkedList encapsulado, para
    facilita o desenvolvimento.
    Uma pilha é uma lista com restrições.

---
+++ src/br/com/caelum/ed/pilhas/Pilha.java
@@ -0,0 +1,13 @@
+package br.com.caelum.ed.pilhas;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import br.com.caelum.ed.Peca;
+
+public class Pilha {
+	
+	private List<Peca> pecas = new LinkedList<>();
+	
+	
+}
