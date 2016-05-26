08c566f77c200c9eff23fc638decc0489e9e8f86
Francisco Chaves <francisco.jschaves@gmail.com>
2016-05-26 19:43:32 -0300

    Fila
    A fila é semelhante a pilha, com a diferência de que entra primeiro e o
    primeiro a sair.

---
+++ src/br/com/caelum/ed/filas/Fila.java
@@ -0,0 +1,12 @@
+package br.com.caelum.ed.filas;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import br.com.caelum.ed.Aluno;
+
+public class Fila {
+
+	private List <Aluno> alunos = new LinkedList<>();
+	
+}
