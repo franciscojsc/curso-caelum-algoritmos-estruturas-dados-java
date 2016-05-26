8afae13741c26f81b6c370254e65208416f7a2ca
Francisco Chaves <francisco.jschaves@gmail.com>
2016-05-26 19:48:43 -0300

    Métodos da FiLa

--- src/br/com/caelum/ed/filas/Fila.java
+++ src/br/com/caelum/ed/filas/Fila.java
@@ -9,4 +9,15 @@ public class Fila {
 
 	private List <Aluno> alunos = new LinkedList<>();
 	
+	public void insere(Aluno aluno){
+		this.alunos.add(aluno);
+	}
+	
+	public Aluno remove(){
+		return this.alunos.remove(0);
+	}
+	
+	public boolean vazia(){
+		return this.alunos.size() == 0;
+	}
 }
